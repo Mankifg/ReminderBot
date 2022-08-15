d5f53281a8b17517f17bc8c360f1af5adc894df8
Mankifg <83959255+Mankifg@users.noreply.github.com>
2022-08-15 16:16:30 +0200

    test commit

--- Cogs/checker.py
+++ Cogs/checker.py
@@ -19,7 +19,7 @@ class tasksCog(commands.Cog, name="ping command"):
     async def check_for_tasks():
         print('abc')   
 
-    @commands.command(name="setchannel", usage="", description="izpise urnik", aliases=['sc'])
+    @commands.command(name="setchannel", usage="", description="Sets channel for preset events.", aliases=['sc'])
     @commands.has_permissions(administrator=True)
     async def setchannel(self, ctx, channel: discord.TextChannel):
         with open('data/channel.txt',"w") as f:
@@ -35,18 +35,13 @@ class tasksCog(commands.Cog, name="ping command"):
             for i in range(len(data['times'])):
                 if data['times'][i] == datetime.datetime.now().strftime("%H:%M"):
                     channel = self.bot.get_channel(int(open('data/channel.txt', 'r').read()))
-                    await channel.send(data['tasks'][i])
+                    await channel.send('@everyone')
                     q = discord.Embed(
-                        title="Urnik",
-                        description="@everyone",
-                        color=discord.Color.random(),
+                        title=f"Task **{data['tasks'][i]}**",
+                        description=f"Time: {data['times'][i]}",
+                        color=discord.Color.dark_blue(),
                     )
-                    q.add_field(
-                        name=data['tasks'][i],
-                        value=f"**```{data['times'][i]}```**",
-                        inline=True,
-                    )
-                    q.set_author(name="Mentorji")
+                    
                     await channel.send(embed=q)
 
                     break
