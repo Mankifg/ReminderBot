c65b72e8961cb635074d108ae726426850340776
Luka Heric <luka.heric@gmail.com>
2022-08-16 13:26:56 +0200

    dodano urejeno dodajanje in popravljen JSON file

--- Cogs/dailySchedule.py
+++ Cogs/dailySchedule.py
@@ -1,10 +1,47 @@
 from ast import alias
+from calendar import WEDNESDAY
 import discord
 from discord.ext import commands, tasks
 from itertools import cycle
 import datetime
 import json
 import asyncio
+import requests, os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+url = "https://weatherbit-v1-mashape.p.rapidapi.com/current"
+
+loc = os.getenv('WEATHER')
+TIME = "10:08"
+
+dnevi = ["ponedeljek", "torek", "sreda", "četrtek", "petek", "sobota", "nedelja"]
+
+def weather():
+    with open('./data/settings.json', 'r') as f:
+        settings = json.load(f)
+    
+    location = settings['place']
+
+    resp = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={location}&limit=1&appid={loc}").json()
+    try:
+        lat = resp[0]['lat']
+        lon = resp[0]['lon']
+    except:
+        lat = 0
+        lon = 0
+
+    querystring = {"lon":lon,"lat":lat}
+
+    headers = {
+        "X-RapidAPI-Key": os.getenv('RAPIDKEY'),
+        "X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring).json()
+    return response,lat,lon
+   
 
 def read():
     with open('./data/schedule.json', 'r') as f:
@@ -17,29 +54,49 @@ class dailyCog(commands.Cog, name="ping command"):
     @commands.Cog.listener()
     async def on_ready(self):
         while True:
-            await asyncio.sleep(30)
+            await asyncio.sleep(10)
             data = read()
 
-            if "8:00" == datetime.datetime.now().strftime("%H:%M"):
+            if True or TIME == datetime.datetime.now().strftime("%H:%M"):
                 channel = self.bot.get_channel(int(open('data/channel.txt', 'r').read()))
                 await channel.send('@everyone')
 
-                stOpravil = len(data["tasks"])
-
                 urnik2 = discord.Embed(
-                    title="Urnik",
+                    title=f"Urnik {datetime.datetime.now().strftime('%d.%m.%Y %H:%M')} - **{dnevi[datetime.datetime.today().weekday()]}**",
                     description="",
                     color=discord.Color.dark_blue(),
                 )
+                w,lat,lon = weather()
+                if not lat == 0 and not lon == 0:
 
-                for i in range(stOpravil):
                     urnik2.add_field(
-                        name=data['tasks'][i],
-                        value=f"**```{data['times'][i]}```**",
-                        inline=True,
+                        name="Temperatura",
+                        value=f"{w['data'][0]['temp']}°C",
+                        inline=False,
                     )
 
-                urnik2.set_author(name="Mentorji")
+                    for task in data["tasks"]:
+                        urnik2.add_field(
+                            name=f"**{task['startTime']}**  {task['title']}",
+                            value=f"ㅤ_{task['description']}_",
+                            inline=False,
+                        )
+                    
+                    urnik2.set_thumbnail(url=f"https://www.weatherbit.io/static/img/icons/{w['data'][0]['weather']['icon']}.png")
+                else:
+                    urnik2.add_field(
+                        name="Temperatura",
+                        value="Ni podatka",
+                        inline=False,
+                    )
+
+                    for task in data["tasks"]:
+                        urnik2.add_field(
+                            name=f"**{task['startTime']}**  {task['title']}",
+                            value=f"ㅤ_{task['description']}_",
+                            inline=False,
+                        )
+
                 await channel.send(embed=urnik2)
 
 
