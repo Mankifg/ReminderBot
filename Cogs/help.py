e345cf852624987e6ee7efc66a65dd342455fcd4
Mankifg <83959255+Mankifg@users.noreply.github.com>
2022-08-17 10:35:57 +0200

    Organizate (#41)
    * black format
    
    * removed unused imports
    
    * settings.json

--- Cogs/help.py
+++ Cogs/help.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
-from random import randint
-import random
+
 
 class HelpCog(commands.Cog, name="help command"):
     def __init__(self, bot: commands.Bot):
